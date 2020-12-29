e4efccb92711b8d65723d187f27902a6cb734939
nberger62 <63067997+nberger62@users.noreply.github.com>
2020-12-29 10:29:57 -0500

    Buffer Script

---
+++ arcpy_analysis.py
@@ -0,0 +1,26 @@
+
+import arcpy
+import pandas
+import numpy
+import scipy
+import ipywidgets
+
+arcpy.env.workspace = r"C:\EsriTraining\CrimeAnalysisInfoProd\Products\Products.gdb"
+
+arcpy.analysis.Buffer(
+    "Crime_Dec2016",
+    r"C:\EsriTraining\CrimeAnalysisInfoProd\Products\Products.gdb\Crime_Dec2016_Buffer3",
+    "100 Meters",
+    "FULL",
+    "ROUND",
+    "NONE",
+    None,
+    "PLANAR"
+)
+
+arcpy.ca.EightyTwentyAnalysis(
+    "Chicago_Crime",
+    r"C:\EsriTraining\CrimeAnalysisInfoProd\Products\Products.gdb\Chicago_Crime_8020Analysis2",
+    None,
+    "Chicago_Crime_District"
+)
